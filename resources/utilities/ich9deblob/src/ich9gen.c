6ff3d7c8e55226ca58b011d870ab993b16576479
Leah Woods <info@minifree.org>
2016-05-14 10:07:47 +0100

    ich9gen ich9gen.c: add missing copyright statement in header

--- resources/utilities/ich9deblob/src/ich9gen.c
+++ resources/utilities/ich9deblob/src/ich9gen.c
@@ -1,5 +1,6 @@
 /*
  *  Copyright (C) 2014, 2015 Leah Woods <info@minifree.org>
+ *  Copyright (C) 2016 Swift Geek <swiftgeek@gmail.com>
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
