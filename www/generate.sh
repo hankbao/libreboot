076e41fe99055bf96e2ec864b69ae5b5685b5f87
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2017-04-08 14:34:08 -0700

    Fix dependency direction

--- www/generate.sh
+++ www/generate.sh
@@ -3,6 +3,6 @@
 [ "x${DEBUG+set}" = 'xset' ] && set -v
 set -e
 
-./index.sh
 find -L . -name '*.html' -and -not -name 'template.html' -delete
 find -L . -name '*.md' -exec ./publish.sh {} \;
+./index.sh
