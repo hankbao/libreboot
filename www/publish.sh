hankbao/libreboot

commit a8d89665de6da20a8793886e03f153e922f6e519

www/publish.sh: Clean up shellcheck warnings
All of the warnings were of type SC2086 [1]: Double quote to prevent
globbing and word splitting.

[1]: https://github.com/koalaman/shellcheck/wiki/Sc2086

www/publish.sh
@@ -70,14 +70,14 @@ TOC=$(grep -q "^x-toc-enable: true$" "$TMPFILE" && printf '%s\n' "--toc --toc-de
 SMART=$(pandoc -v | grep -q '2\.0' || printf '%s\n' "--smart") || SMART=""
 
 # chuck through pandoc
-pandoc $TOC $SMART "$TMPFILE" -s --css /global.css $OPTS \
-        --template template.html --metadata return="$RETURN"> $FILE.html
+pandoc $TOC $SMART "$TMPFILE" -s --css /global.css "$OPTS" \
+        --template template.html --metadata return="$RETURN" > "$FILE.html"
 
 # additionally, produce bare file for RSS
-pandoc $1 > $FILE.bare.html
+pandoc "$1" > "$FILE.bare.html"
 
 # generate section title anchors as [link]
-sed -i -e 's_^<h\([123]\) id="\(.*\)">\(.*\)</h\1>_<div class="h"><h\1 id="\2">\3</h\1><a aria-hidden="true" href="#\2">[link]</a></div>_' $FILE.html
+sed -i -e 's_^<h\([123]\) id="\(.*\)">\(.*\)</h\1>_<div class="h"><h\1 id="\2">\3</h\1><a aria-hidden="true" href="#\2">[link]</a></div>_' "$FILE.html"
 
 # clean up temporary file
 rm -f "$TMPFILE"

commit fee8d605c7208e222a7e0afb6f0f02e392da6e53

www/publish.sh: Fix quoting bug
When running "bash -x publish.sh index.md", the following happens:

    + pandoc --smart /tmp/libreboot_www.Xne7SYgf4e -s --css /global.css '--css /headercenter.css' --template template.html --metadata return=
    pandoc: unrecognized option `--css /headercenter.css'
    Try pandoc --help for more information.

This is because the OPTS variable is being quoted, which causes its
value of "-css /headercenter.css" to not be split on word boundaries,
that is, spaces. Because pandoc has a "--css" option, but not a
"--css /headercenter.css" option, pandoc expectedly complains that
said option does not exist.

To fix this we just unquote OPTS, and add a shellcheck ignore directive
so this won't be reintroduced in the future.

Caused by: a8d89665de6da20a8793886e03f153e922f6e519.

www/publish.sh
@@ -70,7 +70,10 @@ TOC=$(grep -q "^x-toc-enable: true$" "$TMPFILE" && printf '%s\n' "--toc --toc-de
 SMART=$(pandoc -v | grep -q '2\.0' || printf '%s\n' "--smart") || SMART=""
 
 # chuck through pandoc
-pandoc $TOC $SMART "$TMPFILE" -s --css /global.css "$OPTS" \
+#
+# $OPTS must not be quoted, otherwise pandoc interprets '--css /headercenter.css'
+# as one argument, when it is actually two.
+pandoc $TOC $SMART "$TMPFILE" -s --css /global.css $OPTS \
         --template template.html --metadata return="$RETURN" > "$FILE.html"
 
 # additionally, produce bare file for RSS